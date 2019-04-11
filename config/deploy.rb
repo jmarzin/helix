5f27a102e40c137aceefe038cb7e3e93206a2e16
jmarzin <jacques.marzin@free.fr>
2019-04-11 15:52:26 +0200

    maj

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :format, :pretty
 
 ## Defaults:
 # set :scm,           :git
