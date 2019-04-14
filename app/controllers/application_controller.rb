2c84fe4bdaed021e15ff7677a8ac90ce5680d792
jmarzin <jacques.marzin@free.fr>
2019-04-14 22:40:31 +0200

    maj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   def index
-
+    debut = Time.now
+    @resultat = GpxTraite.traite_une_trace(Rails.root.join("public", "gpx", "essai.GPX").to_s)
+    fin = Time.now
+    @temps = fin - debut
   end
 end
